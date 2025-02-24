ef0c23d4c0b25815392e666c19540ae3f39081af
MatheusOliveira04 <Samatheus999@gmail.com>
2025-02-24 19:20:19 -0300

    feat: Created project initial

---
+++ src/test/java/git/MatheusOliveira04/PayFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package git.MatheusOliveira04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
