be80337e8720dbeaf828bcd1d61960420aa361d0
MatheusOliveira04 <Samatheus999@gmail.com>
2025-02-24 20:31:04 -0300

    fix: disabled Spring Security for future implementations

--- src/main/java/git/MatheusOliveira04/PayFlowApplication.java
+++ src/main/java/git/MatheusOliveira04/PayFlowApplication.java
@@ -1,9 +1,13 @@
 package git.MatheusOliveira04;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
 public class PayFlowApplication {
 
 	public static void main(String[] args) {
