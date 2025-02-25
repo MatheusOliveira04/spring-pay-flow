ef0737ca07e1f214322314f87cef70dd39b2db8c
MatheusOliveira04 <Samatheus999@gmail.com>
2025-02-25 18:11:10 -0300

    feat: Created data for initialization database with data

--- src/main/java/git/MatheusOliveira04/PayFlowApplication.java
+++ src/main/java/git/MatheusOliveira04/PayFlowApplication.java
@@ -1,10 +1,14 @@
 package git.MatheusOliveira04;
 
+import git.MatheusOliveira04.models.User;
+import git.MatheusOliveira04.repositories.UserRepository;
+import git.MatheusOliveira04.services.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
@@ -12,6 +16,17 @@ public class PayFlowApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PayFlowApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner initDatabase(UserService userService, UserRepository userRepository) {
+		return args -> {
+			userRepository.deleteAll();
+
+			User user = new User(null, "Matheus", "U@gmail.com", "12345", "USER");
+
+			userRepository.save(user);
+		};
 	}
 
 }
