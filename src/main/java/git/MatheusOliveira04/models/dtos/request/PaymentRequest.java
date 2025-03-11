bab36d62eb18235e2b6d4a429b561f35500cb887
MatheusOliveira04 <Samatheus999@gmail.com>
2025-03-11 15:33:55 -0300

    feat: Added ValueOfEnum validation to PaymentRequest.java

--- src/main/java/git/MatheusOliveira04/models/dtos/request/PaymentRequest.java
+++ src/main/java/git/MatheusOliveira04/models/dtos/request/PaymentRequest.java
@@ -1,6 +1,7 @@
 package git.MatheusOliveira04.models.dtos.request;
 
 import git.MatheusOliveira04.models.enums.PaymentMethod;
+import git.MatheusOliveira04.models.enums.validations.annotations.ValueOfEnum;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
@@ -16,6 +17,7 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 public class PaymentRequest {
     @NotNull
+    @ValueOfEnum(enumClass = PaymentMethod.class)
     String paymentMethod;
     @NotNull @Positive
     BigDecimal amountReceived;
