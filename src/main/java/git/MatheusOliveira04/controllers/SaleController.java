2e4976ed6f0868f290617e47110533c9b1c9a69b
MatheusOliveira04 <Samatheus999@gmail.com>
2025-03-22 13:54:58 -0300

    feat: Added annotation @Operation in SaleController

--- src/main/java/git/MatheusOliveira04/controllers/SaleController.java
+++ src/main/java/git/MatheusOliveira04/controllers/SaleController.java
@@ -5,6 +5,8 @@ import git.MatheusOliveira04.models.dtos.reponse.SalePageResponse;
 import git.MatheusOliveira04.models.dtos.request.SaleRequest;
 import git.MatheusOliveira04.models.mappers.SaleMapper;
 import git.MatheusOliveira04.services.SaleService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Positive;
@@ -29,6 +31,7 @@ public class SaleController {
     @Autowired
     private SaleMapper saleMapper;
 
+    @Operation(security = { @SecurityRequirement(name = "bearer-key") })
     @Secured({"ROLE_USER"})
     @GetMapping
     public ResponseEntity<SalePageResponse> findAll(
@@ -40,12 +43,14 @@ public class SaleController {
         );
     }
 
+    @Operation(security = { @SecurityRequirement(name = "bearer-key") })
     @Secured({"ROLE_USER"})
     @GetMapping("/{id}")
     public ResponseEntity<Sale> findById(@PathVariable UUID id) {
         return ResponseEntity.ok(saleService.findById(id));
     }
 
+    @Operation(security = { @SecurityRequirement(name = "bearer-key") })
     @Secured({"ROLE_ADMIN"})
     @PostMapping
     public ResponseEntity<Sale> insert(@RequestBody @Valid SaleRequest saleRequest, UriComponentsBuilder uriComponentsBuilder) {
@@ -57,6 +62,7 @@ public class SaleController {
                 .body(sale);
     }
 
+    @Operation(security = { @SecurityRequirement(name = "bearer-key") })
     @PutMapping("/{id}")
     public ResponseEntity<Sale> update(@PathVariable UUID id, @RequestBody SaleRequest saleRequest) {
         Sale sale = saleMapper.toSale(saleRequest);
@@ -64,6 +70,7 @@ public class SaleController {
         return ResponseEntity.ok(saleService.update(sale));
     }
 
+    @Operation(security = { @SecurityRequirement(name = "bearer-key") })
     @Secured({"ROLE_ADMIN"})
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable UUID id) {
