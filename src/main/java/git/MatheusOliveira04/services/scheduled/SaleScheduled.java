4ceb9b2053c6b6ef6a714ebedea7c38a08c6ce9d
MatheusOliveira04 <Samatheus999@gmail.com>
2025-03-22 15:44:56 -0300

    feat: Implemented Scheduled in Sale

---
+++ src/main/java/git/MatheusOliveira04/services/scheduled/SaleScheduled.java
@@ -0,0 +1,40 @@
+package git.MatheusOliveira04.services.scheduled;
+
+import git.MatheusOliveira04.models.Sale;
+import git.MatheusOliveira04.models.enums.StatusSale;
+import git.MatheusOliveira04.repositories.SaleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+@Component
+public class SaleScheduled {
+
+    @Autowired
+    private SaleRepository saleRepository;
+
+    @Scheduled(cron = "0 0 0 * * *")
+    @Async
+    public void checkExpirationDateToPay() {
+        saleRepository.findByStatus(StatusSale.DUE).forEach(
+                sale -> {
+                    if (checkDateToPayPassed(sale)) {
+                        updateStatusSaleForNotReceived(sale);
+                        saleRepository.save(sale);
+                    }
+                }
+        );
+    }
+
+    private Boolean checkDateToPayPassed(Sale sale) {
+        return sale.getDateToPay().isAfter(LocalDate.now()) || sale.getDateToPay().isEqual(LocalDate.now());
+    }
+
+    private void updateStatusSaleForNotReceived(Sale sale) {
+        sale.setStatus(StatusSale.NOT_RECEIVED);
+    }
+
+}
