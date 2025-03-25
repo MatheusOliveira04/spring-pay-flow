835751d8e4a00feb9c35d55dc990483c56518bcb
MatheusOliveira04 <Samatheus999@gmail.com>
2025-03-25 18:50:18 -0300

    feat: Created file sql script for user test

---
+++ src/test/resources/sqls/create_users.sql
@@ -0,0 +1,8 @@
+INSERT INTO tb_user (id, email, password, username, roles)
+ VALUES ('53a1e0d8-887b-4c2c-973d-50d03803bd35', 'test1@example.com', '123', 'test 1', ARRAY['USER', 'ADMIN']);
+
+INSERT INTO tb_user (id, email, password, username, roles)
+ VALUES ('53a1e0d8-887b-4c2c-973d-50d03803bd36', 'test2@example.com', '123', 'test 2', ARRAY['USER', 'ADMIN']);
+
+INSERT INTO tb_user (id, email, password, username, roles)
+ VALUES ('53a1e0d8-887b-4c2c-973d-50d03803bd37', 'test3@example.com', '123', 'test 3', ARRAY['USER']);
